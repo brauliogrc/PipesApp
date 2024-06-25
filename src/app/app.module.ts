537dc6bc164d6b5337f2f10609f79773e9454216
Braulio Garcia <Braulio_Garcia@jabil.com>
2024-06-25 13:03:11 -0600

    Initial commit
    Creacion de proyecto
    Instalaciones PrimeNg
    Modulo PrimeNgModule
    Module Shared
    MenuComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
